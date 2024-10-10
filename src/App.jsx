367d333772b6d9fafd6e7cf6475cc83d7d7f77e4
rafay <rafay.ed13@gmail.com>
2024-10-11 04:33:51 +0500

    base files setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+
+import Footer from "./components/layout/footer/Footer";
+import Header from "./components/layout/header/Header";
 import "./styles/style.scss";
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Header></Header>
+
+      <main></main>
+
+      <Footer></Footer>
     </>
   )
 }
