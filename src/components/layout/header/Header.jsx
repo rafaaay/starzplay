15f032470d7ab0004fd1c5468408d53ba895f1d4
rafay <rafay.ed13@gmail.com>
2024-10-20 16:30:07 +0500

    header dropdowns and some ui enchancements added

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+
 function Header() {
+
+    const [openSettings, setOpenSettings] = useState(false);
+    const [openSearch, setOpenSearch] = useState(false);
+
     return (
         <header className="header">
             {/* Header Content */}
@@ -19,18 +25,34 @@ function Header() {
             {/* Header Options */}
             <div className="header__controls">
                 <div className="header__search">
-                    <button aria-label="search" aria-disabled="false" aria-pressed="false">
+                    <button onClick={() => setOpenSearch((prev) => !prev)} aria-label="search" aria-disabled="false" aria-pressed="false">
                         <img src={'/static/icons/icon_search.svg'} />
                     </button>
 
-                    <div className="results"></div>
+                    {
+                        openSearch && 
+                        <div className="search">
+                            <input className="search__input" type="text" name="search" id="search" placeholder="Search" aria-labelledby="search movies or tv series" />
+
+                            <div className="search__results"></div>
+                        </div>
+                    }
                 </div>
 
                 <div className="header__options">
-                    <button aria-label="options" aria-disabled="false" aria-pressed="false">
-                        <img src={'/static/icons/icon_settings.svg'} />
+                    <button onClick={() => setOpenSettings((prev) => !prev)} aria-label="options" aria-disabled="false" aria-pressed="false">
+                        <img src={'/static/icons/icon_settings.svg'} className="wheel" />
                         <img src={'/static/icons/icon_caret-down.svg'} />
                     </button>
+
+                    {
+                        openSettings && 
+                        <ul className="dropdown">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Favorites</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    }
                 </div>
             </div>
             {/* Header Options */}
